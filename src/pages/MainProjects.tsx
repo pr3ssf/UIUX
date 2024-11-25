c4479b7a8a44defa9afc590e6e8b4576cf7f92ba
pressf <alexsapr2003@gmail.com>
2024-11-25 23:34:06 +0300

    dev: main projects 2

--- src/pages/MainProjects.tsx
+++ src/pages/MainProjects.tsx
@@ -39,6 +39,31 @@ const MainProjects = () => {
             </Text>
           </Box>
         </Flex>
+
+        <Flex 
+          direction={{ base: "column", md: "row" }} 
+          alignItems="center" 
+          gap={4} 
+          paddingLeft={10} // Отступ слева
+          paddingRight={10} // Отступ справа
+        >
+          <Box flex="1" textAlign="left"> {/* Описание справа от изображения */}
+            <Text fontSize="lg" fontWeight="bold">Project Title</Text>
+            <Text mt={2}>
+              This is a description of the project. It explains the goals, features, and technology used in this project. 
+              You can add more details here to give viewers a better understanding of what this project is about.
+            </Text>
+          </Box>
+          <Box flex="2" maxWidth="600px"> {/* Задайте максимальную ширину для изображения */}
+            <Image
+              src={yourImage} // Используйте ваше изображение
+              alt="Project Description"
+              boxSize="100%" // Установите ширину изображения на 100% контейнера
+              objectFit="cover" // Обеспечьте правильное отображение
+              borderRadius="8px" // Добавьте закругление углов, если нужно
+            />
+          </Box>
+        </Flex>
       </div>
 
       <div className="buffer"></div>
