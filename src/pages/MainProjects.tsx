c58adfaee2b912ceb8ba41b02476b92b97a7fe0e
pressf <alexsapr2003@gmail.com>
2024-11-25 23:58:30 +0300

    dev: main projects 3

--- src/pages/MainProjects.tsx
+++ src/pages/MainProjects.tsx
@@ -1,6 +1,6 @@
 import { Box, Text, Image, Flex } from '@chakra-ui/react';
 import "../styles/MainProjects.css";
-import yourImage from '../assets/Main Page.png'; // Импортируйте ваше изображение
+import yourImage from '../assets/Main Page.png';
 
 const MainProjects = () => {
   return (
@@ -19,19 +19,19 @@ const MainProjects = () => {
           direction={{ base: "column", md: "row" }} 
           alignItems="center" 
           gap={4} 
-          paddingLeft={10} // Отступ слева
-          paddingRight={10} // Отступ справа
+          paddingLeft={10} 
+          paddingRight={10} 
         >
-          <Box flex="2" maxWidth="600px"> {/* Задайте максимальную ширину для изображения */}
+          <Box flex="2" maxWidth="600px">
             <Image
-              src={yourImage} // Используйте ваше изображение
+              src={yourImage}
               alt="Project Description"
-              boxSize="100%" // Установите ширину изображения на 100% контейнера
-              objectFit="cover" // Обеспечьте правильное отображение
-              borderRadius="8px" // Добавьте закругление углов, если нужно
+              boxSize="100%"
+              objectFit="cover"
+              borderRadius="8px"
             />
           </Box>
-          <Box flex="1" textAlign="left"> {/* Описание справа от изображения */}
+          <Box flex="1" textAlign="left">
             <Text fontSize="lg" fontWeight="bold">Project Title</Text>
             <Text mt={2}>
               This is a description of the project. It explains the goals, features, and technology used in this project. 
@@ -44,29 +44,81 @@ const MainProjects = () => {
           direction={{ base: "column", md: "row" }} 
           alignItems="center" 
           gap={4} 
-          paddingLeft={10} // Отступ слева
-          paddingRight={10} // Отступ справа
+          paddingLeft={10} 
+          paddingRight={10} 
         >
-          <Box flex="1" textAlign="left"> {/* Описание справа от изображения */}
+          <Box flex="1" textAlign="left">
             <Text fontSize="lg" fontWeight="bold">Project Title</Text>
             <Text mt={2}>
               This is a description of the project. It explains the goals, features, and technology used in this project. 
               You can add more details here to give viewers a better understanding of what this project is about.
             </Text>
           </Box>
-          <Box flex="2" maxWidth="600px"> {/* Задайте максимальную ширину для изображения */}
+          <Box flex="2" maxWidth="600px">
             <Image
-              src={yourImage} // Используйте ваше изображение
+              src={yourImage}
               alt="Project Description"
-              boxSize="100%" // Установите ширину изображения на 100% контейнера
-              objectFit="cover" // Обеспечьте правильное отображение
-              borderRadius="8px" // Добавьте закругление углов, если нужно
+              boxSize="100%"
+              objectFit="cover"
+              borderRadius="8px"
             />
           </Box>
         </Flex>
       </div>
 
       <div className="buffer"></div>
+
+      <div className="parallax-container parallax-container2">
+        <Flex 
+          direction={{ base: "column", md: "row" }} 
+          alignItems="center" 
+          gap={4} 
+          paddingLeft={10} 
+          paddingRight={10} 
+        >
+          <Box flex="2" maxWidth="600px">
+            <Image
+              src={yourImage}
+              alt="Project Description"
+              boxSize="100%"
+              objectFit="cover"
+              borderRadius="8px"
+            />
+          </Box>
+          <Box flex="1" textAlign="left">
+            <Text fontSize="lg" fontWeight="bold">Project Title</Text>
+            <Text mt={2}>
+              This is a description of the project. It explains the goals, features, and technology used in this project. 
+              You can add more details here to give viewers a better understanding of what this project is about.
+            </Text>
+          </Box>
+        </Flex>
+
+        <Flex 
+          direction={{ base: "column", md: "row" }} 
+          alignItems="center" 
+          gap={4} 
+          paddingLeft={10} 
+          paddingRight={10} 
+        >
+          <Box flex="1" textAlign="left">
+            <Text fontSize="lg" fontWeight="bold">Project Title</Text>
+            <Text mt={2}>
+              This is a description of the project. It explains the goals, features, and technology used in this project. 
+              You can add more details here to give viewers a better understanding of what this project is about.
+            </Text>
+          </Box>
+          <Box flex="2" maxWidth="600px">
+            <Image
+              src={yourImage}
+              alt="Project Description"
+              boxSize="100%"
+              objectFit="cover"
+              borderRadius="8px"
+            />
+          </Box>
+        </Flex>
+      </div>
     </div>
   );
 };
