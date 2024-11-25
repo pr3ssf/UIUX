727b4c6ba8c60665844bc0059cbeafe6060b6788
pressf <alexsapr2003@gmail.com>
2024-11-25 23:27:26 +0300

    dev: main projects 1

--- src/pages/MainProjects.tsx
+++ src/pages/MainProjects.tsx
@@ -1,62 +1,48 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Image, Flex } from '@chakra-ui/react';
 import "../styles/MainProjects.css";
+import yourImage from '../assets/Main Page.png'; // Импортируйте ваше изображение
 
 const MainProjects = () => {
   return (
     <div>
       <div className="parallax-container">
-      <h1>Main Projects</h1>
-      <p>
-        Main developed environments and levels. 
-      </p>
-    </div>
-
-    <div className="buffer"></div>
-
-    <div className="parallax-container parallax-container2">
-      <div className="card">
-        
-      </div>
-      <div className="card">
-        <div className="card-img"></div>
-        <h3>Rock climbing</h3>
+        <h1>Main Projects</h1>
         <p>
-          The goal is to reach the summit of a formation or the endpoint of a
-          usually pre-defined route without falling
+          Main developed environments and levels. 
         </p>
-        <a href="#">Learn more</a>
       </div>
-      <div className="card">
-        <div className="card-img"></div>
-        <h3>Caving</h3>
-        <p>
-          Exploring underground through networks of tunnels and passageways,
-          which can be natural or artificial.
-        </p>
-        <a href="#">Learn more</a>
+
+      <div className="buffer"></div>
+
+      <div className="parallax-container parallax-container2">
+        <Flex 
+          direction={{ base: "column", md: "row" }} 
+          alignItems="center" 
+          gap={4} 
+          paddingLeft={10} // Отступ слева
+          paddingRight={10} // Отступ справа
+        >
+          <Box flex="2" maxWidth="600px"> {/* Задайте максимальную ширину для изображения */}
+            <Image
+              src={yourImage} // Используйте ваше изображение
+              alt="Project Description"
+              boxSize="100%" // Установите ширину изображения на 100% контейнера
+              objectFit="cover" // Обеспечьте правильное отображение
+              borderRadius="8px" // Добавьте закругление углов, если нужно
+            />
+          </Box>
+          <Box flex="1" textAlign="left"> {/* Описание справа от изображения */}
+            <Text fontSize="lg" fontWeight="bold">Project Title</Text>
+            <Text mt={2}>
+              This is a description of the project. It explains the goals, features, and technology used in this project. 
+              You can add more details here to give viewers a better understanding of what this project is about.
+            </Text>
+          </Box>
+        </Flex>
       </div>
-      <div className="card">
-        <div className="card-img"></div>
-        <h3>Paragliding</h3>
-        <p>
-          Get lift off from the mountain and enjoy through the splendor of the
-          surrounding landscape by parachute.
-        </p>
-        <a href="#">Learn more</a>
-      </div>
+
+      <div className="buffer"></div>
     </div>
-
-    <div className="buffer"></div>
-
-    </div>
-    
-
-  //   <Box p={5} height="100%">
-  //     <Box h="10000px" />
-  //     <Text fontSize="xl" color="brand.800">
-  //       Main Projects
-  //     </Text>
-  //   </Box>
   );
 };
 
