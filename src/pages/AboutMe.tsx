57836b260b9227020661ec9d4ac635ca8c41ae13
pressfk <90253595+pr3ssf@users.noreply.github.com>
2024-12-01 23:51:33 +0300

    DEV-1: New AboutMe & CleanUp (#2)
    * dev-1: aboutMe-1
    
    * dev-1: aboutMe-2
    
    * dev-1: aboutMe-3
    
    * dev-1: aboutMe-4 self
    
    * dev-1: aboutMe-4 last
    
    * dev-1: global cleanup

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,74 +1,236 @@
-import { Box, Heading, Text, Image, UnorderedList, ListItem, Link, Flex } from "@chakra-ui/react";
+import { 
+  Box, 
+  Heading, 
+  Text, 
+  Image, 
+  UnorderedList, 
+  ListItem, 
+  Link, 
+  Flex 
+} from "@chakra-ui/react";
 import gojoImage from '../assets/gojo.png'; // Импортируем изображение
+import "../styles/AboutMe.css";
 
 const AboutMe = () => {
   return (
-    <Box
-    maxWidth='100%'
-    // bg='brand.200'
-     >
-      <Box h="100px" />
-      <Box 
-        maxWidth="70%" 
-        mx="auto" 
-        p={4} 
-        fontFamily="Arial" 
-        color="brand.400"
-        //bg='brand.200' 
+    <div>
+      {/* Параллакс контейнер 1 */}
+      <div className="parallax-container">
+        <h1>About Me</h1>
+        <p style={{ textAlign: "center" }}>
+          Quickly and efficiently complete tasks, including complex and unconventional cases.
+        </p>
+      </div>
+
+      <div className="buffer"></div>
+
+      {/* Параллакс контейнер 2 */}
+      <div className="parallax-container parallax-container2">
+        <div className="container-element" style={{ width: "100%", margin: "0 auto" }}>
+          <div className="element-text" style={{ margin: "0 auto" }}>
+            <div className="element-text-title">Alexander Saprykin</div>
+            <div className="element-text-description">
+              Generalist / Lead 3D<br />
+              Moscow<br />
+              Age: 21 years old
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+
+          <div>
+            <div className="element-text">
+              {/* Incomplete Higher Education */}
+              <div>
+                <div className="title" style={{ textAlign: "center" }}>
+                  Incomplete Higher Education
+                </div>
+                <div className="description">
+                  <strong>2025: Moscow Technical University of Communications and Informatics</strong>
+                </div>
+                <div className="description">
+                  Information technology, Computer science and engineering
+                </div>
+              </div>
+
+              {/* Professional Development, Courses */}
+              <div style={{ marginTop: "20px" }}>
+                <div className="title" style={{ textAlign: "center" }}>
+                  Professional Development, Courses
+                </div>
+                <UnorderedList className="description" style={{ fontWeight: "normal" }}>
+                  <ListItem>
+                    <strong>2024: Intro To Motion Graphics</strong> — Ducky 3D, Motion design in Blender
+                  </ListItem>
+                  <ListItem>
+                    <strong>2024: Fusion 360 Course</strong> — Udemy, CAD modelling in Fusion 360
+                  </ListItem>
+                  <ListItem>
+                    <strong>2021: 3D Modeller</strong> — 3D Club, Modelling and visualization in 3ds Max
+                  </ListItem>
+                  <ListItem>
+                    <strong>2021: Hard Surface</strong> — Blender Bros, Hard-surface modelling in Blender
+                  </ListItem>
+                  <ListItem>
+                    <strong>2021: PBR Texture Trip</strong> — XYZ School, Texturing in Substance Painter/Designer
+                  </ListItem>
+                </UnorderedList>
+              </div>
+            </div>
+
+            <div className="element-text">
+              <p className="element-text-title">Skills</p>
+              <UnorderedList className="element-text-description">
+                <ListItem>
+                  <strong>Advanced:</strong> Blender, Marmoset Toolbag, Unity, Adobe Substance 3D Painter
+                </ListItem>
+                <ListItem>
+                  <strong>Skillful:</strong> Adobe Photoshop, Git, Unreal Engine, RizomUV
+                </ListItem>
+                <ListItem>
+                  <strong>Normal:</strong> Adobe Substance 3D Designer, ZBrush
+                </ListItem>
+              </UnorderedList>
+            </div>
+          </div>
+        </div>
+
+        <div className="container-element"></div>
+      </div>
+
+      <div className="buffer"></div>
+
+      {/* Параллакс контейнер 3 */}
+      <div className="parallax-container parallax-container3">
+        <div
+          className="container-element"
+          style={{ display: "flex", flex: "1", flexDirection: "column", width: "100%" }}
         >
-        <Flex mb={10} justify="space-between" align="center">
-          <Box textAlign="left">
-            <Heading as="h1" size="xl" mb={1}>
-              Alexander Saprykin (pressf)
-            </Heading>
-            <Text fontSize="lg">3D Construction Designer & 3D Artist</Text>
-            <Text fontSize="md" color="gray.600">Work experience: 3 years</Text>
-          </Box>
-          <Image
-            src={gojoImage} // Используем импортированное изображение
-            alt="Alexander Saprykin"
-            borderRadius="full"
-            boxSize="150px"
-            objectFit="cover"
-            ml={4}
-          />
-        </Flex>
+          <div className="element-text" style={{ width: "100%", textAlign: "center" }}>
+            <h1>Work Experience</h1>
+          </div>
 
-        <Box mb={10} textAlign="left"> {/* Выравнивание по левому краю */}
-          <Heading as="h2" size="lg" mb={4}>Current Workplace</Heading>
-          <Box mb={6}>
-            <Heading as="h3" size="md">MAGNA ELECTRONICS LIMITED LIABILITY COMPANY</Heading>
-              <Text><strong>Position:</strong> 3D Construction Designer (Multiplayer VR projects)</Text>
-              <Text><strong>Responsibilities:</strong></Text>
+          <div className="element-text" style={{ width: "100%" }}>
+            <div className="title">MAGNA ELECTRONICS (10.2023 — Present)</div>
+            <div>
+              <strong>Position:</strong> Lead 3D
+            </div>
+            <div>
+              <strong>Experience:</strong>
+            </div>
             <UnorderedList>
-              <ListItem>Level design and locations creation (Blender + Unity)</ListItem>
-              <ListItem>Full pipeline model creation</ListItem>
-              <ListItem>PBR texturing</ListItem>
-              <ListItem>Optimization of models and scenes</ListItem>
+              <ListItem>Level design and level art for multiplayer maps (from concept to whitebox).</ListItem>
+              <ListItem>
+                Modeling in a realistic setting using AAA/Atlas pipelines with LOD support and rendering optimization.
+              </ListItem>
+              <ListItem>Full model integration in Unity, including retopology and UV mapping.</ListItem>
+              <ListItem>Creation and use of single, tile, atlas, and trim textures.</ListItem>
+              <ListItem>Development of shaders, VFX, post-processing, and particle systems.</ListItem>
+              <ListItem>Optimization of scenes, models, and lighting, including setup and light baking.</ListItem>
+              <ListItem>
+                Task management: tracking, reviewing, and assigning tasks for junior 3D modelers.
+              </ListItem>
+              <ListItem>
+                Collaboration with the backend team on engine-related questions and player-world interactions.
+              </ListItem>
             </UnorderedList>
-          </Box>
-        </Box>
+            <div>
+              <strong>Achievements:</strong>
+            </div>
+            <UnorderedList>
+              <ListItem>
+                Developed 6 levels in a realistic setting with full integration of environment and lighting.
+              </ListItem>
+              <ListItem>Created over 50 hard-surface models across various pipelines.</ListItem>
+              <ListItem>Designed 9 shaders for gameplay effects and network interactions.</ListItem>
+              <ListItem>Prepared commercial project for publication on marketplaces.</ListItem>
+            </UnorderedList>
+          </div>
+        </div>
+      </div>
 
-        <Box mb={10} textAlign="left"> {/* Выравнивание по левому краю */}
-          <Heading as="h2" size="lg" mb={4}>Skills</Heading>
-          <UnorderedList>
-            <ListItem>Blender 3D</ListItem>
-            <ListItem>Unity</ListItem>
-            <ListItem>Adobe Substance 3D Painter</ListItem>
-            <ListItem>UV Mapping, Retopology</ListItem>
-            <ListItem>Git, Responsibility, Learnability</ListItem>
-          </UnorderedList>
-        </Box>
+      <div className="buffer"></div>
 
-        <Box mb={10} textAlign="left"> {/* Выравнивание по левому краю */}
-          <Heading as="h2" size="lg" mb={4}>Portfolio</Heading>
-          <Text>
-            Behance: <Link href="https://www.behance.net/alex_saprykin" isExternal color="blue.500">alex_saprykin</Link>
-          </Text>
-        </Box>
-        <Box h="100px" />
-      </Box>
-    </Box>
+      {/* Параллакс контейнер 4 */}
+      <div className="parallax-container parallax-container4">
+        <div
+          className="container-element"
+          style={{ display: "flex", flex: "1", flexDirection: "column", width: "100%" }}
+        >
+          <div className="element-text" style={{ width: "100%" }}>
+            <div className="title">Freelance</div>
+            <div>
+              <strong>Generalist 3D</strong>
+            </div>
+            <UnorderedList>
+              <ListItem>
+                Full cycle of 3D model and level development for VR and multiplayer projects, including concept
+                creation, modeling, setup, and optimization.
+              </ListItem>
+              <ListItem>
+                Participation in projects: VR shooter in a closed location, drone assembly and testing training, drone
+                racing for Yandex Games.
+              </ListItem>
+              <ListItem>
+                Collaboration with development teams, integration of models and assets into game engines (Unreal Engine
+                5, Unity).
+              </ListItem>
+              <ListItem>
+                Industrial and interior design, creation of models for visualization and compositing.
+              </ListItem>
+              <ListItem>
+                Support for game projects, including level design, graphics optimization, and custom model development.
+              </ListItem>
+            </UnorderedList>
+          </div>
+
+          <div className="element-text" style={{ width: "100%" }}>
+            <div className="title">Contacts</div>
+            <div>
+              Телефон: <strong >+7 915 560-80-64</strong>
+            </div>
+            <div>
+              Email: <strong>pressf.dev@gmail.com</strong>
+            </div>
+            <div>
+              Telegram:{" "}
+              <a href="https://t.me/pr3ss_f" target="_blank" rel="noopener noreferrer">
+                <strong>https://t.me/pr3ss_f</strong>
+              </a>
+            </div>
+            <div>
+              ArtStation:{" "}
+              <a href="https://artstation.com/pressfk" target="_blank" rel="noopener noreferrer">
+                <strong>artstation.com/pressfk</strong>
+              </a>
+            </div>
+            <div>
+              Behance:{" "}
+              <a href="https://behance.net/pressfk" target="_blank" rel="noopener noreferrer">
+                <strong>behance.net/pressfk</strong>
+              </a>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+
+      <div className="buffer"></div>
+    </div>
   );
 };
 
--- src/pages/MainProjects.tsx
+++ src/pages/MainProjects.tsx
@@ -1,26 +1,26 @@
 import "../styles/MainProjects.css";
-import yourImage from '../assets/Main Page.png';
+import yourImage from "../assets/Main Page.png";
 
 const MainProjects = () => {
   return (
     <div>
+      {/* Header Section */}
       <div className="parallax-container">
         <h1>Main Projects</h1>
-        <p>
-          Main developed environments and levels.
-        </p>
+        <p>Main developed environments and levels.</p>
       </div>
 
       <div className="buffer"></div>
 
+      {/* Section 1 */}
       <div className="parallax-container parallax-container2">
-        <div className="container-element ">
+        <div className="container-element">
           <div style={{ flex: 2, maxWidth: "600px" }}>
             <img src={yourImage} alt="Project Description" className="element-image" />
           </div>
-          <div className='element-text'>
+          <div className="element-text">
             <p className="element-text-title">Project Title</p>
-            <p className='element-text-description'>
+            <p className="element-text-description">
               This is a description of the project. It explains the goals, features, and technology used in this project. 
               You can add more details here to give viewers a better understanding of what this project is about.
             </p>
@@ -28,9 +28,9 @@ const MainProjects = () => {
         </div>
 
         <div className="container-element">
-          <div className='element-text'>
+          <div className="element-text">
             <p className="element-text-title">Project Title</p>
-            <p className='element-text-description'>
+            <p className="element-text-description">
               This is a description of the project. It explains the goals, features, and technology used in this project. 
               You can add more details here to give viewers a better understanding of what this project is about.
             </p>
@@ -43,14 +43,15 @@ const MainProjects = () => {
 
       <div className="buffer"></div>
 
+      {/* Section 2 */}
       <div className="parallax-container parallax-container3">
         <div className="container-element">
           <div style={{ flex: 2, maxWidth: "600px" }}>
             <img src={yourImage} alt="Project Description" className="element-image" />
           </div>
-          <div className='element-text'>
+          <div className="element-text">
             <p className="element-text-title">Project Title</p>
-            <p className='element-text-description'>
+            <p className="element-text-description">
               This is a description of the project. It explains the goals, features, and technology used in this project. 
               You can add more details here to give viewers a better understanding of what this project is about.
             </p>
@@ -58,9 +59,9 @@ const MainProjects = () => {
         </div>
 
         <div className="container-element">
-          <div className='element-text'>
+          <div className="element-text">
             <p className="element-text-title">Project Title</p>
-            <p className='element-text-description'>
+            <p className="element-text-description">
               This is a description of the project. It explains the goals, features, and technology used in this project. 
               You can add more details here to give viewers a better understanding of what this project is about.
             </p>
@@ -73,14 +74,15 @@ const MainProjects = () => {
 
       <div className="buffer"></div>
 
+      {/* Section 3 */}
       <div className="parallax-container parallax-container4">
         <div className="container-element">
           <div style={{ flex: 2, maxWidth: "600px" }}>
             <img src={yourImage} alt="Project Description" className="element-image" />
           </div>
-          <div className='element-text'>
+          <div className="element-text">
             <p className="element-text-title">Project Title</p>
-            <p className='element-text-description'>
+            <p className="element-text-description">
               This is a description of the project. It explains the goals, features, and technology used in this project. 
               You can add more details here to give viewers a better understanding of what this project is about.
             </p>
@@ -88,9 +90,9 @@ const MainProjects = () => {
         </div>
 
         <div className="container-element">
-          <div className='element-text'>
+          <div className="element-text">
             <p className="element-text-title">Project Title</p>
-            <p className='element-text-description'>
+            <p className="element-text-description">
               This is a description of the project. It explains the goals, features, and technology used in this project. 
               You can add more details here to give viewers a better understanding of what this project is about.
             </p>
@@ -102,7 +104,6 @@ const MainProjects = () => {
       </div>
 
       <div className="buffer"></div>
-      
     </div>
   );
 };
